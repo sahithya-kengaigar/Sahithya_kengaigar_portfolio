10c5f1397d37914e468fc5febcf28ab7d4c92c5d
SAHITHYA KENGAIGAR <89730674+sahithya-kengaigar@users.noreply.github.com>
2024-03-30 16:53:20 -0500

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,36 @@
+var wrapBox = $("#type-js");
+var typingText = $(".text-js");
+var newString = "";
+const typingSpeed = 200;
+
+var autoType = () => {
+
+  let text = wrapBox.text().trim().split('');
+
+  wrapBox.prepend('<div class="cursor" style="right: initial; left:0;"></div>');
+  wrapBox.find(".text-js");
+
+  setTimeout(function() {
+
+    typingText.css("opacity", 1);
+    typingText.prev().removeAttr("style");
+    typingText.text("");
+
+    let amntOfChars = text.length;
+
+    for (var i = 0; i < amntOfChars; i++) {
+
+      var char = text[i];
+
+      getChar(i + 1, text[i]);
+    }
+
+  }, 1500);
+};
+
+var getChar = (i, char) => {
+  setTimeout(function() {
+    newString += char;
+    typingText.text(newString);
+  }, i * typingSpeed);
+};
